918b57da18069ffb47e03c831fb818056213efc8
Ayush <ayush421301@gmail.com>
2024-10-19 14:49:26 +0530

    Added Colour Code in tailwind.config File

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ["Montserrat", "sans-serif"],
+      },
+      backgroundImage: {
+        'proj': 'linear-gradient(to right, #0d9488, #06b6d4)', // Teal-green gradient
+        'proj-hover': 'linear-gradient(to right, #0a6f6f, #0ea5e5)', // Slightly darker gradient for hover
+      },
+    },
+  },
+  variants: {
+    extend: {
+      backgroundImage: ['hover'],
+    },
   },
   plugins: [],
 };
